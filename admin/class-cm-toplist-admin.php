5c9b5371e89269ebcdb30b48a24e5f32e5deff53
Jabatronic <jabatronic@gmail.com>
2020-03-30 01:36:29 +0100

    remove trailing comma causing crash on activation!

--- admin/class-cm-toplist-admin.php
+++ admin/class-cm-toplist-admin.php
@@ -66,7 +66,7 @@ class Cm_Toplist_Admin {
 			'manage_options',           // Capabilities.
 			'cm-toplist-admin',         // Menu Slug.
 			array( $this, 'cm_display_page' ), // Function.
-			'5',                        // Menu Position.
+			'5'                         // Menu Position.
 		);
 	}
 
