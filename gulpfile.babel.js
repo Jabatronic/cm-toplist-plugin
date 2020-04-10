aa00f3d1c215d80ff6583af3ad608364929d8005
Jabatronic <jabatronic@gmail.com>
2020-04-10 18:32:19 +0100

    linting and refactoring

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,87 +1,114 @@
 // /* eslint-env es6 */
 // /* eslint no-undef: "off" */
-'use strict';
-
+"use strict";
 
 /**
- * TODO: Minify JS, Add browsersync for live reload in dev,
- * remove unused packages
+ * TODO: Minify JS, remove unused packages
  */
 
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
-const rename        = require('gulp-rename');
-const path          = require('path');
-const postcss       = require('gulp-postcss');
-const autoprefixer  = require('autoprefixer');
-const cssnano       = require('cssnano');
-const fs            = require('fs');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const path = require("path");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const fs = require("fs");
 // const plumber       = require('gulp-plumber');
-const del           = require('del');
-const babel         = require('gulp-babel');
+const del = require("del");
+const babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
+const cache = require("gulp-cache");
 const processors = [
-    //postcss plugins go here
-    autoprefixer({grid: true}),
-    cssnano
+  //postcss plugins go here
+  autoprefixer({ grid: true }),
+  cssnano,
 ];
-
 
 /**
  * SCSS
  */
 const adminSCSS = () => {
-    return gulp.src("./admin/css/sass/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        .pipe(rename(function(path){
-            path.basename += '.min'
-        }))
-        .pipe(gulp.dest("./admin/css"))
-}
+  return gulp
+    .src("./admin/css/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(processors))
+    .pipe(
+      rename(function (path) {
+        path.basename += ".min";
+      })
+    )
+    .pipe(gulp.dest("./admin/css"))
+    .pipe(browserSync.stream());
+};
 
 const publicSCSS = () => {
-    return gulp.src("./public/css/sass/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        .pipe(rename(function(path){
-            path.basename += '.min'
-        }))
-        .pipe(gulp.dest("./public/css"))
-}
+  return gulp
+    .src("./public/css/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(processors))
+    .pipe(
+      rename(function (path) {
+        path.basename += ".min";
+      })
+    )
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
+};
 
 /**
  * JS
  */
 const adminJS = () => {
-    return gulp.src("./admin/js/src/*.js")
-    .pipe( babel() )
-    .pipe(rename(function(path){
-        path.basename += '.es5'
-    }))
-    .pipe( gulp.dest("./admin/js/") )
-}
+  return gulp
+    .src("./admin/js/src/*.js")
+    .pipe(babel())
+    .pipe(
+      rename(function (path) {
+        path.basename += ".es5";
+      })
+    )
+    .pipe(gulp.dest("./admin/js/"))
+    .pipe(browserSync.stream());
+};
 
-const watchSCSS = () => {
-    gulp.watch(['admin/css/sass/*.scss', 'public/css/sass/*.scss'], gulp.series(exports.adminSCSS, exports.publicSCSS));
-}
+const publicJS = () => {
+  return gulp
+    .src("./public/js/src/*.js")
+    .pipe(babel())
+    .pipe(
+      rename(function (path) {
+        path.basename += ".es5";
+      })
+    )
+    .pipe(gulp.dest("./public/js/"))
+    .pipe(browserSync.stream());
+};
 
-const watchJS = () => {
-    gulp.watch(['admin/js/src/*.js'], gulp.series(exports.adminJS));
-}
+// Browser sync
+const serve = () => {
+  browserSync.init({
+    proxy: "http://localhost/jtron-plugin-dev",
+    // notify: false,
+  });
 
-const watchAll = () => {
-    // Watch the lot of 'em
-    gulp.watch(['admin/css/sass/*.scss', 'public/css/sass/*.scss', 'admin/js/src/*.js'], gulp.series(adminSCSS, publicSCSS, adminJS));
-}
+  gulp.watch(
+    ["admin/css/sass/*.scss", "public/css/sass/*.scss"],
+    gulp.series(adminSCSS, publicSCSS)
+  );
+  gulp
+    .watch(
+      ["admin/js/src/*.js", "public/js/src/*.js"],
+      gulp.series(adminJS, publicJS)
+    )
+    .on("change", browserSync.reload);
+  gulp.watch("./**/*.php").on("change", browserSync.reload);
+};
 
-const defaultTasks = gulp.series(adminSCSS, publicSCSS, adminJS, watchAll);
-
+const defaultTasks = gulp.series(serve);
 
 exports.adminSCSS = adminSCSS;
 exports.publicSCSS = publicSCSS;
 exports.adminJS = adminJS;
-exports.watchSCSS = watchSCSS;
-exports.watchJS = watchJS;
-exports.watchAll = watchAll;
 
 exports.default = defaultTasks;
